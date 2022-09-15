0af8d8e7913c048cfb05a44eeb6a322ef90c7c22
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-15 19:53:35 -0400

    Create IREBATE.sol

---
+++ development/interfaces/IREBATE.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IREBATE {
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function withdraw() external returns (bool);
+    function withdrawETH() external returns (bool);
+    function withdrawToken(address token) external returns (bool);
+    function transfer(uint256 eth, address payable receiver) external returns (bool success);
+}
